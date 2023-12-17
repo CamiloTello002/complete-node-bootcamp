0ad4820c5460b5aa2f4aeb3e08d1a83fa201da54
CamiloTello002 <camilojtello5@gmail.com>
2023-12-16 21:07:25 -0500

    bug server to be fixed

--- 2-how-node-works/starter/events.js
+++ 2-how-node-works/starter/events.js
@@ -1,3 +1,46 @@
 const EventEmitter = require('events');
+const http = require('http');
 
-const myEmitter = new EventEmitter();
+class Sales extends EventEmitter{
+    constructor() {
+        super();
+    }
+}
+
+const myEmitter = new Sales();
+
+myEmitter.on('newSale', () => {
+    console.log("a sale has been done :3")
+})
+
+myEmitter.on('newSale', () => {
+    console.log("costumer name: Juliana")
+})
+
+myEmitter.on('newSale', stock => {
+    console.log(`There are now ${stock} items left in stock`)
+})
+
+myEmitter.emit('newSale', 9)
+
+/////////////////////////////////
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+    console.log('request received')
+    res.write('request received');
+    res.end('yes')
+})
+
+server.on('request', (req, res) => {
+    res.write('another request hah');
+})
+
+server.on('close', (req, res) => {
+    res.end('server closed haha');
+})
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('were waiting for requests haha')
+})
