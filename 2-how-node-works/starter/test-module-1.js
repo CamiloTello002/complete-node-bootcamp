389d82fd4932a316bd681efa233be38e593ff317
CamiloTello002 <camilojtello5@gmail.com>
2023-12-18 10:13:10 -0500

    exporting and caching

--- 2-how-node-works/starter/test-module-1.js
+++ 2-how-node-works/starter/test-module-1.js
@@ -10,5 +10,20 @@ class Calculator {
     }
 }
 
-
+// module.exports for one value
 module.exports = Calculator;
+
+// another way would be:
+/*
+module.exports = class {
+    add(a, b) {
+        return a + b;
+    }
+    multiply(a, b) {
+        return a * b;
+    }
+    divide(a, b) {
+        return a / b;
+    }
+}
+*/
