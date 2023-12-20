1173532aa03f846f34de34678cb8805630240d12
DiamondSplash <camilojtello5@gmail.com>
2023-12-20 10:20:31 -0500

    something doesn't work

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,10 +1,44 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
+// takes the file name as the argument
+const readFilePro = file => {
+  // promises take executor functions
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find the file you wanted')
+      resolve(data) // any value we pass to resolve is what we'll return
+    })
+  })
+}
+
+readFilePro(`${__dirname}/dog.txt`)
+// piece of code to be executed in case the promise was fulfilled
+.then(data => {
+    console.log(`Breed is: ${data}`);
+
+    superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .then( res => {
+        console.log(res.body.message);
+        fs.writeFile('dog-img.txt', res.body.message, err => {
+          if (err) return console.log(err.message);
+          console.log('Random image has been saved to file :)')
+        });
+    })
+    .catch(err => {
+        console.log(err.message);
+    });
+  });
+
+
+/*
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+    // file information is shown
     console.log(`Breed: ${data}`);
 
     superagent
+    // this get method returns a promise
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
     .end((err, res) => {
         if (err) return console.log(err.message)
@@ -15,3 +49,4 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
         });
     });
 })
+*/
