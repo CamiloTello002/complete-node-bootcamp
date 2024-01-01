2cebe2311032238fddb604e3e406cca046c1a8fa
CamiloTello002 <camilojtello5@gmail.com>
2023-12-31 23:30:32 -0500

    working with promises :)

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -10,25 +10,43 @@ const readFilePro = file => {
   });
 };
 
-// dog.txt must NOT contain a newline, else it won't work
-readFilePro(`${__dirname}/dog.txt`).then(data => {
-    console.log(`Breed: ${data}`);
+// function that runs in the background
+const getDogPic = async () => {
+    try{
+    // when the promise is resolved, it'll save the value in the data variable
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed name: ${data}`)
 
-    superagent
-        .get(`https://dog.ceo/api/breed/${data}/images/random`)
-        .then(res => {
-            console.log(res.body.message);
+    const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+    console.log(res.body.message);
 
-            fs.writeFile('dog-img.txt', res.body.message, err => {
-                if (err) return console.log(err.message);
-                console.log('Random dog saved to file');
-            });
-        })
-        .catch(err => {
-            console.log(err.message);
-        });
-})
+    await writeFilePro('dog-img.txt', res.body.message);
+    console.log('random dog saved to file!!');
+    } catch (err) {
+        console.log(err);
+    }
+}
+getDogPic();
 
+/*
+// dog.txt must NOT contain a newline, else it won' work
+readFilePro(`${__dirname}/dog.txt`)
+    .then(data => {
+        console.log(`Breed: ${data}`);
+        return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+    })
+    .then(res => {
+        console.log(res.body.message);
+        return writeFilePro('dog-img.txt', res.body.message)
+    })
+    .then(() => {
+        console.log('Random dog image saved to file :)')
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+*/
 const writeFilePro = (file, data) => {
   return new Promise((resolve, reject) => {
     fs.writeFile(file, data, err => {
