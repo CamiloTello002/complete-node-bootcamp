d7f1afafb8030d16060bf9dde115f6eeb12ad0d0
CamiloTello002 <camilojtello5@gmail.com>
2024-01-01 11:24:42 -0500

    getting return values from promises

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -13,20 +13,46 @@ const readFilePro = file => {
 // function that runs in the background
 const getDogPic = async () => {
     try{
-    // when the promise is resolved, it'll save the value in the data variable
+    // reads contents of file
     const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed name: ${data}`)
-
+    // get request with contents of file
     const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
     console.log(res.body.message);
-
+    // takes the message of the response's body and saves it in a file
     await writeFilePro('dog-img.txt', res.body.message);
     console.log('random dog saved to file!!');
+    // in case something fails, it'll throw an error
     } catch (err) {
         console.log(err);
+        throw(err);
     }
-}
-getDogPic();
+  return '2: READY';
+};
+(async () => {
+  try{
+    console.log('step 1: get dog pics');
+    console.log(await getDogPic());
+    console.log('step 3: done!!!');
+  } catch(err) {
+    console.log('ERROR!!!');
+  }
+})();
+/*
+console.log("1: We'll get dog pics");
+getDogPic()
+  // in case promise is fulfilled
+  .then(x => {
+    console.log(x);
+    console.log('3: done getting dog pics');
+})
+  // error handler
+  .catch( err => {
+    console.log('ERROR');
+});
+*/
+
+
 
 /*
 // dog.txt must NOT contain a newline, else it won' work
