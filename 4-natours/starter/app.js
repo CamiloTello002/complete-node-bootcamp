9299d8fe0209c0eb45ac42c7ebc5b0bf63e1aec1
CamiloTello002 <camilojtello5@gmail.com>
2024-01-11 07:45:08 -0500

    handling post requests

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,12 +1,48 @@
 // methods that will come in handy
-const app = require('express')();
+const express = require('express');
+const fs = require('fs');
 
-app.get('/', (req, res) => {
-    res.status(404).json({title: "hola a todos", message: "la verdad es que Kandy Bania me gusta", app:"Natours"});
+const app = express();
+
+//app.get('/', (req, res) => {
+//    res.status(404).json({title: "hola a todos", message: "la verdad es que Kandy Bania me gusta", app:"Natours"});
+//});
+//
+//app.post('/', (req, res) => {
+//    res.send('You can post to this endpoint...');
+//});
+
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'))
+
+// first endpoint. Specifying api version is recommended
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        length: tours.length,
+        data: {
+            tours
+        }
+    });
 });
 
-app.post('/', (req, res) => {
-    res.send('You can post to this endpoint...');
+// adds data to tours
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+    console.log(newTour);
+    console.log(req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    });
 });
 
 const port = 3000;
