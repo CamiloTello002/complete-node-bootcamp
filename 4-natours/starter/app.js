907db7daf34a2e800c35a3b100de0ca1167a1e46
CamiloTello002 <camilojtello5@gmail.com>
2024-01-16 09:01:15 -0500

    refactoring routing

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -4,53 +4,32 @@ const fs = require('fs');
 
 const app = express();
 
-//app.get('/', (req, res) => {
-//    res.status(404).json({title: "hola a todos", message: "la verdad es que Kandy Bania me gusta", app:"Natours"});
-//});
-//
-//app.post('/', (req, res) => {
-//    res.send('You can post to this endpoint...');
-//});
-
 app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
 );
 
-// first endpoint. Specifying api version is recommended
-app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    length: tours.length,
-    data: {
-      tours,
-    },
-  });
-});
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
 
-// id and x are mandatory, y is optional
-app.get('/api/v1/tours/:id', (req, res) => {
-  console.log(req.params);
-  // console.log(tours.length)
-  const tour = tours.find((el) => el.id === parseInt(req.params.id));
-  console.log(tour);
-  if (!tour) {
+const deleteTour = (req, res) => {
+  // in case tour doesn't exist
+  if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
       message: 'Invalid ID',
     });
   }
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-});
+  // 204 means no content
+  console.log('hey its done haha');
+  res.status(204).json({ status: 'success', data: null });
+};
 
-// adds data to tours
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
@@ -69,9 +48,38 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+const getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    length: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+const getTour = (req, res) => {
+  console.log(req.params);
+  // console.log(tours.length)
+  const tour = tours.find((el) => el.id === parseInt(req.params.id));
+  console.log(tour);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+const updateTour = (req, res) => {
   // in case tour doesn't exist
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
@@ -82,21 +90,15 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   res
     .status(200)
     .json({ status: 'success', data: { tour: '<updated tour here...>' } });
-});
+};
 
-// deleting a resource
-app.delete('/api/v1/tours/:id', (req, res) => {
-  // in case tour doesn't exist
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  // 204 means no content
-  console.log('hey its done haha');
-  res.status(204).json({ status: 'success', data: null });
-});
+// simpler way to define routes
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 3000;
 
