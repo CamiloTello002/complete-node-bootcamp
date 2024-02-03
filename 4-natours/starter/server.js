85f4fa1a596da86c5700ee8660c924e086e737c9
CamiloTello002 <camilojtello5@gmail.com>
2024-02-02 20:51:13 -0500

    our backend actually interacts with MongoDB

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,7 +1,46 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
+const { Double, Decimal128 } = require('mongodb');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB).then((con) => {
+  console.log('Successful :)');
+});
+
+const toursSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  price: { type: Number, required: [true, 'A tour must have a price'] },
+  rating: { type: Number, default: 4.5 },
+});
+
+const TourModel = mongoose.model('tours', toursSchema);
+
+const testTour = new TourModel({
+  name: 'Parinacochas con Raimondi',
+  price: 258,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// const tourcreated = new TourModel({ name: 'the blinding light' });
 
 const port = process.env.PORT || 3000;
 // 3) STARTS SERVER now
