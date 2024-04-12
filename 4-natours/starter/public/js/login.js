cc0d6f3ce2a747ca22b90cc813b98c8dfe868e60
CamiloTello002 <camilojtello5@gmail.com>
2024-04-12 12:52:54 -0500

    lesson 190 finished

---
+++ 4-natours/starter/public/js/login.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+/* 
+  remember that elements will be selected according to where this js file is called
+*/
+
+const login = async (email, password) => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://localhost:3000/api/v1/users/login',
+      data: {
+        email,
+        password,
+      },
+    });
+    window.location.assign('/');
+  } catch (err) {
+    alert(err.response.data.message);
+  }
+};
+
+document.querySelector('.form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  login(email, password);
+});
--- 4-natours/starter/public/js/logout.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-const logoutButton = document.querySelector('.nav__el--logout');
-logoutButton.addEventListener('click', logOut);
-function logOut() {
-  console.log(`The cookies are: ${document.cookie}`);
-}
