60cea1ad22095b6a8ba707259a70e897e223bf1a
CamiloTello002 <camilojtello5@gmail.com>
2024-04-13 14:01:25 -0500

    feat: handle error for missing tour Display a user-friendly message to inform the user

--- 4-natours/starter/controllers/viewController.js
+++ 4-natours/starter/controllers/viewController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.getOverview = catchAsync(async (req, res) => {
   // 1) Get tour data from our collection
@@ -13,12 +14,16 @@ exports.getOverview = catchAsync(async (req, res) => {
     tours,
   });
 });
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // 1) Get the data
   const tour = await Tour.findOne({ slug: req.params.tourSlug }).populate({
     path: 'reviews',
     fields: 'review rating user',
   });
+  // 2) if a tour is not found, then throw an error
+  if (!tour) return next(new AppError('There is no tour with that name!', 404));
+
+  // 3) if found, then return it
   res.status(200).render('tour', {
     title: `${tour.name} Tour`,
     tour,
