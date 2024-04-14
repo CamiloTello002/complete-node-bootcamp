527613bfa0ac4b3b171e654c638d23b3e246f2e3
CamiloTello002 <camilojtello5@gmail.com>
2024-04-14 15:14:44 -0500

    image uploading works with storage and filter configurations

--- 4-natours/starter/controllers/userController.js
+++ 4-natours/starter/controllers/userController.js
@@ -1,9 +1,36 @@
 // const fs = require('fs');
+const multer = require('multer');
 const User = require('../models/userModel');
 // const APIFeatures = require('../utils/apiFeatures');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
+
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/img/users');
+  },
+  filename: (req, file, cb) => {
+    // user-7889a-22323243.jpeg
+    const ext = file.mimetype.split('/')[1];
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  },
+});
+
+const multerFilter = (req, file, cb) => {
+  // this is for testing if the uploaded file
+  // is actually an image
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images.', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
 
 const filterObj = (obj, changeableFields) => {
   const updatedObj = {};
@@ -39,6 +66,8 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
 });
 
 exports.updateMe = catchAsync(async (req, res, next) => {
+  console.log(req.file);
+  console.log(req.body);
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
@@ -72,3 +101,5 @@ exports.getMe = (req, res, next) => {
   req.params.id = req.user.id;
   next();
 };
+
+exports.uploadUserPhoto = upload.single('photo');
